d118c33992fd5896f305a8e4b81174fbd439182f
kaifalam2002 <kaifalam12a@gmail.com>
2025-10-19 18:20:29 +0530

    k

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
+//This is developed by Kaif
 @SpringBootApplication
 @EnableScheduling
 public class DemoApplication {
